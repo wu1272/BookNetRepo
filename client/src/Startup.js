e569ae4608a44ffbc831a01c14f8f73933aa89a1
Glen Michael Austin Eder <geder@purdue.edu>
2020-03-04 15:53:17 -0500

    Removes uneeded css code from startup module

--- client/src/Startup.js
+++ client/src/Startup.js
@@ -8,8 +8,8 @@ class Startup extends Component {
         <section className="wrapper">
           <div class="form-wrapper">
             <h1>Welcome to BookNet!</h1>
-            <button class="btn" onClick={() => window.location.href = '/login'}>Login</button>
-            <button class="btn2" onClick={() => window.location.href = '/signup'}>Signup</button>
+            <button onClick={() => window.location.href = '/login'}>Login</button>
+            <button onClick={() => window.location.href = '/signup'}>Signup</button>
           </div>
         </section>
       </body>
