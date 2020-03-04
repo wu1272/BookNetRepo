80f21e63dcc946288fb1fcd50a22c621441ac60a
Glen Michael Austin Eder <geder@purdue.edu>
2020-03-04 15:28:29 -0500

    Removes non class from css file

--- client/src/Startup.js
+++ client/src/Startup.js
@@ -5,8 +5,8 @@ class Startup extends Component {
   render() {
     return (
       <body>
-        <section class="outer">
-          <div class="inner">
+        <section className="wrapper">
+          <div class="form-wrapper">
             <h1>Welcome to BookNet!</h1>
             <button class="btn" onClick={() => window.location.href = '/login'}>Login</button>
             <button class="btn2" onClick={() => window.location.href = '/signup'}>Signup</button>
