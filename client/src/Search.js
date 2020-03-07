3caa75a4df992f6ff35a95a074831d1d6bed7425
wu1272 <wu1272@users.noreply.github.com>
2020-03-07 18:09:19 -0500

    added search functionality from Google Books API

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,20 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import app from "./base.js";
 import axios from "axios";
+import styles from "./search.module.css"
 
-class Search extends Component {
-    componentDidMount(){
-        setBooksNeeded("998", "Book of Life", "John Deere");
-    }
 
-    render () {
+function Search() {
+        const[book, setBook] = useState("");
+        const[result, setResult] = useState([]);
+        const[apiKey, setApiKey] = useState("AIzaSyDRic7TFZjyUeYGPtUo0UVj1Wzx1oXhAsc");
+        function handleChange(event) {
+            const book = event.target.value;
+            setBook(book);
+        }
+        function handleSubmit(event){
+            event.preventDefault();
+            axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key=" + apiKey+"&maxResults=40")
+            .then(data=> {
+                console.log(data);
+                setResult(data.data.items);
+            })
+        }
+        //setBooksNeeded("998", "Book of Life", "John Deere");
         return (
+            <body className="landing">
             <div>
-                <h1>added book of life</h1>
+                <h1>Book Search App</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input onChange={handleChange} type="text" placeholder="Search for Books" autoComplete="off"/>
+                    </div>
+                    <button className={styles.tester} type="submit">Search</button>
+                </form>
+                {result.map(book => (
+                    <a href={book.volumeInfo.previewLink}>
+                    <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+                    </a>
+                ))}
             </div>
+            </body>
         );
-    }
-}
+        }
 
 
 function setBooksNeeded(ISBN, title, author) {
